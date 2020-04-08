3ecf1eac33c79721a37a6074c7a4c170d0e14c10
Angelika Nowicka <a.nowicka@i-systems.pl>
2020-04-08 22:54:17 +0200

    Convert csv to json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'productListing';
+
+  constructor(
+    private http: HttpClient) { }
+
+  ngOnInit() {
+    const qwe = this.http.get('../assets/data/dane_pl.csv', { responseType: 'text' });
+    qwe.subscribe(d => {
+      console.log('qwe', this.csvJSON(d));
+    });
+  }
+
+  csvJSON(csv: string){
+
+    const lines = csv.split('\n');
+    const result = [];
+    const headers = lines.shift().split(';');
+
+    lines.forEach((asd, i) => {
+      console.log('asd', asd);
+      const obj = {};
+      const currentline = asd.split(';');
+
+      console.log('curr', currentline);
+
+      headers.forEach((zxc, j) => {
+        obj[zxc] = currentline[j];
+      });
+
+	    result.push(obj);
+    });
+
+    return result;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
